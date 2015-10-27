8a8b1492a0c4bb0a1dca769fb401a3df44150675
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-10-27 15:01:59 +0100

    Add results and indirect facts

--- corpo_recommender.pl
+++ corpo_recommender.pl
@@ -3,7 +3,111 @@
 :- dynamic([xpozytywne/2, xnegatywne/2]).
 
 
+jest_to(tester) :-
+  zna(programowanie),
+  zna(testowanie),
+  zna(metodyki_wytwarzania_oprogramowania),
+  jest(dokladny).
 
+jest_to(programista) :-
+  zna(programowanie),
+  zna(metodyki_wytwarzania_oprogramowania).
+
+jest_to(project_menadzer) :-
+  zna(metodyki_wytwarzania_oprogramowania),
+  zna(praca_w_grupie),
+  jest(negocjator),
+  jest(wizjoner),
+  jest(asertywny),
+  jest(lider).
+
+jest_to(architekt) :-
+  jest_to(programista),
+  jest_to(tester).
+
+jest_to(pr) :-
+  zna(praca_w_grupie),
+  jest(negocjator),
+  jest(wizjoner),
+  jest(esteta),
+  jest(asertywny).
+
+jest_to(administrator) :-
+  zna(sieci).
+
+jest_to(analityk) :-
+  zna(statystyka),
+  jest(eksperymentator).
+
+jest_to(badacz) :-
+  zna(programowanie),
+  zna(statystyka),
+  jest(eksperymentator),
+  jest(ryzykant).
+
+jest_to(hr) :-
+  jest(negocjator),
+  jest(wizjoner),
+  jest(esteta),
+  jest(asertywny).
+
+jest_to(grafik) :-
+  zna(photoshop),
+  jest(dokladny),
+  jest(esteta).
+
+
+zna(praca_w_grupie) :-
+  pozytywne(zna, praca_w_grupie).
+
+zna(programowanie) :-
+  pozytywne(zna, programowanie).
+
+zna(testowanie) :-
+  pozytywne(zna, testowanie).
+
+zna(metodyki_wytwarzania_oprogramowania) :-
+  pozytywne(zna, metodyki_wytwarzania_oprogramowania).
+
+zna(photoshop) :-
+  pozytywne(zna, photoshop).
+
+zna(statystyka) :-
+  pozytywne(zna, statystyka).
+
+zna(sieci) :-
+  pozytywne(zna, sieci).
+
+jest(asertywny) :-
+  pozytywne(jest, asertywny).
+
+jest(negocjator) :-
+  pozytywne(dobrze, negocjuje).
+
+jest(esteta) :-
+  pozytywne(projektuje, ładne_rzeczy).
+
+jest(dokladny) :-
+  pozytywne(jest, dokładny).
+
+jest(odporny_na_stres) :-
+  pozytywne(jest, odporny_na_stres).
+
+jest(ryzykant) :-
+  pozytywne(lubi, ryzyko).
+
+jest(eksperymentator) :-
+  pozytywne(lubi, eksperymentowac).
+
+jest(lider) :-
+  jest(wizjoner),
+  pozytywne(sie, angazuje).
+
+jest(elastyczny) :-
+  negatywne(lubi, sztywne_godziny_pracy).
+
+jest(wizjoner) :-
+  pozytywne(mysli, przyszlosciowo).
 
 
 pozytywne(X, Y) :-
@@ -54,7 +158,7 @@ wyczysc_fakty :-
   readln(_).
 
 wykonaj :-
-  pracownik_jest(X), !,
+  jest_to(X), !,
   write('Twoj pracownik moze byc '), write(X), nl,
   wyczysc_fakty.
 
