84926b4308888073d62fee38397647f72b4f0b53
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-12-08 17:11:49 +0100

    Fix how

--- corpo_recommender.pl
+++ corpo_recommender.pl
@@ -1,14 +1,14 @@
 :- module(corpo_recommender,[wykonaj]).
 
-:- dynamic([xpozytywne/2, xnegatywne/2, xjest/1, xjest_to/1, xwhy/2, xhow/3]).
+:- dynamic([xpozytywne/2, xnegatywne/2, xjest/1, xjest_to/1, xwhy/3, xhow/5]).
 
 przygotuj :-
   assertz(xpozytywne('', '')),
   assertz(xnegatywne('', '')),
   assertz(xjest('')),
   assertz(xjest_to('')),
-  assertz(xwhy('','')),
-  assertz(xhow('','','')).
+  assertz(xwhy('','', '')),
+  assertz(xhow('','','','','')).
 
 kjest_to(X) :-
   jest_to(X),
@@ -17,161 +17,181 @@ kjest_to(X) :-
   assertz(xwhy('jest_to', X)))).
 
 jest_to(tester_manualny) :-
-  kjest(tester),
-  kjest(eksperymentator),
-  pozytywne(jest, cierpliwy).
+  kjest(tester_manualny, tester),
+  kjest(tester_manualny, eksperymentator),
+  pozytywne(tester_manualny, jest, cierpliwy).
 
 jest_to(tester_automatyczny) :-
-  kjest(tester),
-  pozytywne(zna, programowanie),
-  pozytywne(zna, metodyki_wytwarzania_oprogramowania).
+  kjest(tester_automatyczny, tester),
+  pozytywne(tester_automatyczny, zna, programowanie),
+  pozytywne(tester_automatyczny, zna, metodyki_wytwarzania_oprogramowania).
 
 jest_to(programista) :-
-  kjest(umyslem_scislym),
-  pozytywne(zna, programowanie),
-  pozytywne(zna, metodyki_wytwarzania_oprogramowania),
-  pozytywne(jest, schematyczny).
+  kjest(programista, umyslem_scislym),
+  pozytywne(programista, zna, programowanie),
+  pozytywne(programista, zna, metodyki_wytwarzania_oprogramowania),
+  pozytywne(programista, jest, schematyczny).
 
 jest_to(project_menadzer) :-
-  kjest(lider),
-  kjest(negocjator),
-  pozytywne(zna, metodyki_wytwarzania_oprogramowania).
+  kjest(project_manadzer, lider),
+  kjest(project_manadzer, negocjator),
+  pozytywne(project_manadzer, zna, metodyki_wytwarzania_oprogramowania).
 
 jest_to(architekt) :-
-  kjest_to(programista),
-  kjest(tester),
-  pozytywne(ma, duzy_staz).
+  kjest_to(architekt, programista),
+  kjest(architekt, tester),
+  pozytywne(architekt, ma, duzy_staz).
 
 jest_to(frontend) :-
-  kjest(esteta),
-  kjest(wizjoner),
-  pozytywne(jest, cierpliwy),
-  pozytywne(sie, angazuje).
+  kjest(frontend, esteta),
+  kjest(frontend, wizjoner),
+  pozytywne(frontend, jest, cierpliwy),
+  pozytywne(frontend, sie, angazuje).
 
 jest_to(backend) :-
-  kjest(umyslem_scislym),
-  pozytywne(jest, dokladny),
-  pozytywne(zna, programowanie).
+  kjest(backend, umyslem_scislym),
+  pozytywne(backend, jest, dokladny),
+  pozytywne(backend, zna, programowanie).
 
 jest_to(producent) :-
-  kjest(wizjoner),
-  kjest(negocjator),
-  not(jest(ryzykant)),
-  pozytywne(ma, duzy_staz).
+  kjest(producent, wizjoner),
+  kjest(producent, negocjator),
+  not(kjest(producent, ryzykant)),
+  pozytywne(producent, ma, duzy_staz).
 
 jest_to(pr) :-
-  kjest(wizjoner),
-  kjest(esteta),
-  pozytywne(zna, praca_w_grupie),
-  pozytywne(dobrze, negocjuje),
-  pozytywne(jest, asertywny).
+  kjest(pr, wizjoner),
+  kjest(pr, esteta),
+  pozytywne(pr, zna, praca_w_grupie),
+  pozytywne(pr, dobrze, negocjuje),
+  pozytywne(pr, jest, asertywny).
 
 jest_to(administrator) :-
-  pozytywne(zna, sieci).
+  pozytywne(administrator, zna, sieci).
 
 jest_to(analityk) :-
-  kjest(eksperymentator),
-  pozytywne(zna, statystyka).
+  kjest(analityk, eksperymentator),
+  pozytywne(analityk, zna, statystyka).
 
 jest_to(badacz) :-
-  kjest(ryzykant),
-  kjest(eksperymentator),
-  pozytywne(zna, programowanie),
-  pozytywne(zna, statystyka).
+  kjest(badacz, ryzykant),
+  kjest(badacz, eksperymentator),
+  pozytywne(badacz, zna, programowanie),
+  pozytywne(badacz, zna, statystyka).
 
 jest_to(hr) :-
-  kjest(wizjoner),
-  kjest(esteta),
-  pozytywne(dobrze, negocjuje),
-  pozytywne(jest, asertywny).
+  kjest(hr, wizjoner),
+  kjest(hr, esteta),
+  pozytywne(hr, dobrze, negocjuje),
+  pozytywne(hr, jest, asertywny).
 
 jest_to(grafik) :-
-  kjest(esteta),
-  pozytywne(zna,photoshop).
+  kjest(grafik, esteta),
+  pozytywne(grafik, zna, photoshop).
 
 
 
-kjest(X) :-
-  assertz(xwhy('jest', X)),
-  jest(X),
-  (xjest(X);
-  assertz(xjest(X))).
+kjest(X, Y) :-
+  assertz(xwhy('jest_to', '_', X)),
+  assertz(xwhy('jest', X, Y)),
+  jest(X, Y),
+  (xjest(Y);
+  assertz(xjest(Y))).
 
-jest(tester) :-
-  jest(umyslem_scislym),
-  pozytywne(jest, dokladny),
-  pozytywne(zna, testowanie).
+jest(X, tester) :-
+  kjest(X, umyslem_scislym),
+  pozytywne(X, tester, jest, dokladny),
+  pozytywne(X, tester, zna, testowanie).
 
-jest(negocjator) :-
-  pozytywne(jest, asertywny),
-  pozytywne(jest, odporny_na_stres),
-  pozytywne(zna, praca_w_grupie),
-  pozytywne(dobrze, negocjuje),
-  pozytywne(lubi, samorozwoj).
+jest(X, negocjator) :-
+  pozytywne(X, negocjator, jest, asertywny),
+  pozytywne(X, negocjator, jest, odporny_na_stres),
+  pozytywne(X, negocjator, zna, praca_w_grupie),
+  pozytywne(X, negocjator, dobrze, negocjuje),
+  pozytywne(X, negocjator, lubi, samorozwoj).
 
-jest(esteta) :-
-  pozytywne(projektuje, ladne_rzeczy),
-  pozytywne(jest, dokladny).
+jest(X, esteta) :-
+  pozytywne(X, esteta, projektuje, ladne_rzeczy),
+  pozytywne(X, esteta, jest, dokladny).
 
-jest(ryzykant) :-
-  pozytywne(lubi, ryzyko),
-  negatywne(patrzy, przyszlosciowo).
+jest(X, ryzykant) :-
+  pozytywne(X, ryzykant, lubi, ryzyko),
+  negatywne(X, ryzykant, patrzy, przyszlosciowo).
 
-jest(eksperymentator) :-
-  pozytywne(lubi, ryzyko),
-  pozytywne(patrzy, przyszlosciowo).
+jest(X, eksperymentator) :-
+  pozytywne(X, eksperymentator, ryzyko),
+  pozytywne(X, eksperymentator, przyszlosciowo).
 
-jest(lider) :-
-  jest(wizjoner),
-  pozytywne(sie, angazuje),
-  pozytywne(jest, asertywny).
+jest(X, lider) :-
+  kjest(X, wizjoner),
+  pozytywne(X, lider, sie, angazuje),
+  pozytywne(X, lider, jest, asertywny).
 
-jest(elastyczny) :-
-  negatywne(lubi, sztywne_godziny_pracy),
-  negatywne(jest, schematyczny).
+jest(X, elastyczny) :-
+  negatywne(X, elastyczny, lubi, sztywne_godziny_pracy),
+  negatywne(X, elastyczny, jest, schematyczny).
 
-jest(wizjoner) :-
-  pozytywne(patrzy, przyszlosciowo),
-  pozytywne(lubi, eksperymentowac),
-  negatywne(jest, schematyczny).
+jest(X, wizjoner) :-
+  pozytywne(X, wizjoner, patrzy, przyszlosciowo),
+  pozytywne(X, wizjoner, lubi, eksperymentowac),
+  negatywne(X, wizjoner, jest, schematyczny).
 
-jest(umyslem_scislym) :-
-  pozytywne(umie, matematyke);
-  pozytywne(mysli, logicznie).
+jest(X, umyslem_scislym) :-
+  pozytywne(X, umyslem_scislym, umie, matematyke);
+  pozytywne(X, umyslem_scislym, mysli, logicznie).
 
 
-pozytywne(X, Y) :-
+pozytywne(_, _, X, Y) :-
+  assertz(xwhy('pozytywne', X, Y)),
   xpozytywne(X, Y), !.
 
-pozytywne(X, Y) :-
+pozytywne(H, S, X, Y) :-
   not(xnegatywne(X, Y)),
-  pytaj(X, Y, tak).
+  pytaj(H, S, X, Y, tak).
 
-negatywne(X, Y) :-
+pozytywne(_, X, Y) :-
+  assertz(xwhy('pozytywne', X, Y)),
+  xpozytywne(X, Y), !.
+
+pozytywne(H, X, Y) :-
+  not(xnegatywne(X, Y)),
+  pytaj(H, H, X, Y, tak).
+
+negatywne(_, _, X, Y) :-
+  assertz(xwhy('negatywne', X, Y)),
   xnegatywne(X, Y), !.
 
-negatywne(X, Y) :-
+negatywne(H, S, X, Y) :-
   not(xpozytywne(X, Y)),
-  pytaj(X, Y, nie).
+  pytaj(H, S, X, Y, nie).
 
-pytaj(X, Y, K) :-
+negatywne(_, X, Y) :-
+  assertz(xwhy('negatywne', X, Y)),
+  xnegatywne(X, Y), !.
+
+negatywne(H, X, Y) :-
+  not(xpozytywne(X, Y)),
+  pytaj(H, H, X, Y, nie).
+
+
+pytaj(H, S, X, Y, K) :-
   !, write(X), write(' nasz_pracownik '), write(Y), write(' ? (t/n)\n'),
   readln([Replay]),
 
-  (pamietaj(X, Y, Replay),
+  (pamietaj(H, S, X, Y, Replay),
   odpowiedz(Replay, K);
 
   (odpowiedz(Replay, what),
-  pytaj(X, Y, K));
+  pytaj(H, S, X, Y, K));
 
   (odpowiedz(Replay, how),
-  pytaj(X, Y, K));
+  pytaj(H, S, X, Y, K));
 
   (odpowiedz(Replay, why),
-  pytaj(X, Y, K));
+  pytaj(H, S, X, Y, K));
 
   (odpowiedz(Replay, help),
-  pytaj(X, Y, K))).
+  pytaj(H, S, X, Y, K))).
 
 odpowiedz(Replay, tak) :-
   sub_string(Replay, 0, _, _, 't').
@@ -201,15 +221,15 @@ help :-
   write('how - lista what poszerzona o sposob w jaki uzyskano dany fakt (odpowiedz na symptom, reguly)\n'),
   write('why - dlaczego system pyta o dany symptom (stos regul przetwarzanych, ktore wygenerowaly pytanie)\n').
 
-pamietaj(X, Y, Replay) :-
+pamietaj(H, S, X, Y, Replay) :-
   odpowiedz(Replay, tak),
   assertz(xpozytywne(X, Y)),
-  assertz(xhow(X, Y, 'tak')).
+  assertz(xhow(H, S, X, Y, 'tak')).
 
-pamietaj(X, Y, Replay) :-
+pamietaj(H, S, X, Y, Replay) :-
   odpowiedz(Replay, nie),
   assertz(xnegatywne(X, Y)),
-  assertz(xhow(X, Y, 'nie')).
+  assertz(xhow(H, S, X, Y, 'nie')).
 
 podaj_what :-
   write('WHAT:\n'),
@@ -233,20 +253,20 @@ podaj_what :-
 podaj_why :-
   write('WHY:\n'),
   write('Stos zapytan:\n'),
-  bagof([K00, K01], xwhy(K00,K01), L00),
-  drukuj_bez_glowy(L00),
+  bagof([K0, K1, K2], xwhy(K0, K1, K2), L0),
+  drukuj_bez_glowy(L0),
   write('KONIEC WHY\n').
 
 podaj_how :-
   podaj_what,
   write('HOW:\n'),
   write('Skad sie wziely fakty posrednie:\n'),
-  % bagof([Fakt1, Fakt2, Fakt3], (xhow(Fakt1, Fakt2, Fakt3), xjest(Fakt1)), B1),
-  bagof([Fakt1, Fakt2, Fakt3], (xhow(Fakt1, Fakt2, Fakt3)), B1),
+  bagof([F1, F2, F3, F4, F5], (xhow(F1, F2, F3, F4, F5), xjest(F2)), B1),
+  % bagof([Fakt1, Fakt2, Fakt3], (xhow(Fakt1, Fakt2, Fakt3)), B1),
   drukuj_bez_glowy(B1),
   write('\nSkad sie wziely hipotezy:\n'),
   % bagof([H1, H2, H3], (xhow(H1, H2, H3), xjest_to(H1)), B2),
-  bagof([H1, H2, H3], (xhow(H1, H2, H3)), B2),
+  bagof([H1, H2, H3, H4, H5], (xhow(H1, H2, H3, H4, H5), xjest_to(H1)), B2),
   drukuj_bez_glowy(B2),
   write('KONIEC HOW\n').
 
